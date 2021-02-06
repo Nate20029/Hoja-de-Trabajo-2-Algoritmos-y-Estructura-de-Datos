53f3b1cae97e52597c3b40cca6887680c0dd5c25
Nate20029 <70552363+Nate20029@users.noreply.github.com>
2021-02-05 22:58:40 -0600

    Add files via upload

---
+++ DriverHT2.java
@@ -0,0 +1,20 @@
+//--------------------------------------------------------------
+//Hoja de Trabajo #2
+//Creada por Natanael Girón 20029
+//Algoritmos y Estructura de Datos
+//Ultima modificación: 05/02/2021
+//-----------------------------------------------------------------
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+public class DriverHT2 {
+     public static void main(String[] args) {
+          
+          Scanner archivo = new Scanner(new File(datos.txt));
+          System.out.println(archivo);
+
+     }
+}
