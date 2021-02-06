a2222090e0d352139edf1626805689b5425b3340
Nate20029 <70552363+Nate20029@users.noreply.github.com>
2021-02-05 22:56:43 -0600

    Add files via upload

---
+++ CalculadoraGeneral.java
@@ -0,0 +1,12 @@
+//--------------------------------------------------------------
+//Hoja de Trabajo #2
+//Creada por Natanael Girón 20029
+//Algoritmos y Estructura de Datos
+//Ultima modificación: 05/02/2021
+//-----------------------------------------------------------------
+
+public interface CalculadoraGeneral {
+     
+     //Método encargado de realizar todas las operaciones
+     public String Calculo(String expresion);
+}
